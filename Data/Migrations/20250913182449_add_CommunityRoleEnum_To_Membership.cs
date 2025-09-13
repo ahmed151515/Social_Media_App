f623224346e1d310ceaabfa6c3d86cb472b653b4
Ahmed Arafa <ahmedarafa.200444@gmail.com>
2025-09-13 22:48:39 +0300

    .

---
+++ Data/Migrations/20250913182449_add_CommunityRoleEnum_To_Membership.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_CommunityRoleEnum_To_Membership : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CommunityRole",
+                table: "Memberships",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommunityRole",
+                table: "Memberships");
+        }
+    }
+}
